73b00613205c199bba4dd738330bc238eb7c04e0
lttkid <2831355712@qq.com>
2024-08-23 16:06:17 +0800

    Site updated: 2024-08-23 16:06:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/23/hello-world/","2024/08/23/page/"];function toRandomPost(){
+var posts=["2024/08/23/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
