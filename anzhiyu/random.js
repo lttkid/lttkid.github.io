9032009998393e04b8c126ea4ad9e84f4be8ae0d
lttkid <2831355712@qq.com>
2024-08-24 11:03:05 +0800

    Site updated: 2024-08-24 11:03:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/23/hello-world/","2024/08/23/第一次做静态博客界面/","2024/08/24/web前端/"];function toRandomPost(){
+var posts=["2024/08/23/hello-world/","2024/08/23/第一次做静态博客界面/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
