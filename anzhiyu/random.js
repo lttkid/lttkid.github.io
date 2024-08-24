c61c662fd390799a711d3fa11baccc5026b9c96b
lttkid <2831355712@qq.com>
2024-08-24 21:44:48 +0800

    Site updated: 2024-08-24 21:44:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/23/hello-world/","2024/08/23/第一次做静态博客界面/"];function toRandomPost(){
+var posts=["2024/08/23/hello-world/","2024/08/23/第一次做静态博客界面/","2024/08/24/近期打算/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
